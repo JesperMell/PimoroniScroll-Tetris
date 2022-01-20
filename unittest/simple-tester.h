2abf37303c8723f7b52e2c10478f9e282c2bca33
JesperMell <jesper.mellquist@hotmail.com>
2022-01-20 06:36:01 +0100

    working on unit test

--- unittest/simple-tester.h
+++ unittest/simple-tester.h
@@ -1,7 +1,15 @@
 #ifndef _SIMPLE_TESTER_H_
 #define _SIMPLE_TESTER_H_
 
-#define TEST(name, a,b) (if((uint32_t)a != (uint32_t)b) {\
-    printf("Failed to test %s: expected: %d, actual %d", name, a, b));
+#include<stdint.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+#define TEST(name, a,b)              \
+  ({                                 \
+    if((uint32_t)a != (uint32_t)b) { \
+      printf("Failed %s: expected: %d, actual %d", name, a, b); \
+  });\
+  };
 
 #endif 
--- unittest/unittest.cpp
+++ unittest/unittest.cpp
@@ -1,6 +1,9 @@
+#include<simple-tester.h>
+
 int main()
 {
 
+  TEST("simple",1,1)
 
   return 0;
 }
